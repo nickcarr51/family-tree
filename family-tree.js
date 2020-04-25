91e083debaef1ccf934268ba9725133bdf4735be
nickcarr51 <nick.carr84@gmail.com>
2020-04-25 13:09:30 -0400

    042520 01

--- family-tree.js
+++ family-tree.js
@@ -25,25 +25,104 @@ class FamilyTree {
   }
 
   log() {
-    logCalls++;
+    logCalls++
     let tree = `-- ${this.value}\n`;
     if (this.children.length > 0) {
       this.children.forEach(function(child) {
         if (child.children.length > 0) {
-          tree += `--${child.log()}`
-          logCalls--
+          for (let i = 0; i < logCalls; i++) {
+            tree += `--`;
+          }                   
+          tree += `${child.log()}`
         } else {
           for (let i = 0; i < logCalls; i++) {
-            tree += `--`
+            tree += `--`;
           }
-          tree += `-- ${child.value}\n`
-        }
+            tree += `-- ${child.value}\n`;
+          }
       })
     }
+    logCalls--;
     return tree;
   }
 
+  //   log() {
+
+  //   let logCalls = 0;
+    
+  //   return (logCalls) => {
+  //     logCalls++
+  //     let tree = `-- ${this.value}\n`;
+  //     if (this.children.length > 0) {
+  //       this.children.forEach(function(child) {
+  //         if (child.children.length > 0) {
+  //           for (let i = 0; i < logCalls; i++) {
+  //             tree += `--`;
+  //           }                   
+  //           tree += `${child.log()}`
+  //         } else {
+  //           for (let i = 0; i < logCalls; i++) {
+  //             tree += `--`;
+  //           }
+  //             tree += `-- ${child.value}\n`;
+  //           }
+  //       })
+  //     }
+  //     logCalls--;
+  //     return tree;
+  //   }
+
+  // }
 }
+
+// class FamilyTree {
+//   constructor (value) {
+//     if (typeof value !== 'string') throw console.error();
+//     this.value = value;
+//     this.children = [];
+//     this.logCalls = 0;
+//   }
+
+//   insert(child) {
+//     child = new FamilyTree(child);
+//     this.children.push(child);
+//   }
+
+//   familySize() {
+//     return this.children.length + 1;
+//   }
+
+//   findMember(member) {
+//     for (let i = 0; i < this.children.length; i++) {
+//       if (this.children[i].value === member) {
+//         return this.children[i];
+//       }        
+//     }
+//   }
+
+//   log() {
+//     this.logCalls++;
+//     let tree = `-- ${this.value}\n`;
+//     if (this.children.length > 0) {
+//       this.children.forEach(function(child) {
+//         if (child.children.length > 0) {
+//           for (let i = 0; i < this.logCalls; i++) {
+//             tree += `--`;
+//           }                   
+//           tree += `${child.log()}`
+//         } else {
+//           for (let i = 0; i < this.logCalls; i++) {
+//             tree += `--`;
+//           }
+//             tree += `-- ${child.value}\n`;
+//           }
+//       })
+//     }
+//     this.logCalls--;
+//     return tree;
+//   }
+// }
+
 
 
 module.exports = FamilyTree;
