afc7e04e426faeee4c89465189a2880b12a9bf8c
nickcarr51 <nick.carr84@gmail.com>
2020-04-27 18:26:57 -0400

    completed homework

--- family-tree.js
+++ family-tree.js
@@ -23,7 +23,7 @@ class FamilyTree {
       }        
     }
   }
-
+  
   log() {
     logCalls++
     let tree = `-- ${this.value}\n`;
@@ -46,82 +46,9 @@ class FamilyTree {
     return tree;
   }
 
-  //   log() {
-
-  //   let logCalls = 0;
-    
-  //   return (logCalls) => {
-  //     logCalls++
-  //     let tree = `-- ${this.value}\n`;
-  //     if (this.children.length > 0) {
-  //       this.children.forEach(function(child) {
-  //         if (child.children.length > 0) {
-  //           for (let i = 0; i < logCalls; i++) {
-  //             tree += `--`;
-  //           }                   
-  //           tree += `${child.log()}`
-  //         } else {
-  //           for (let i = 0; i < logCalls; i++) {
-  //             tree += `--`;
-  //           }
-  //             tree += `-- ${child.value}\n`;
-  //           }
-  //       })
-  //     }
-  //     logCalls--;
-  //     return tree;
-  //   }
-
-  // }
 }
 
-// class FamilyTree {
-//   constructor (value) {
-//     if (typeof value !== 'string') throw console.error();
-//     this.value = value;
-//     this.children = [];
-//     this.logCalls = 0;
-//   }
 
-//   insert(child) {
-//     child = new FamilyTree(child);
-//     this.children.push(child);
-//   }
-
-//   familySize() {
-//     return this.children.length + 1;
-//   }
-
-//   findMember(member) {
-//     for (let i = 0; i < this.children.length; i++) {
-//       if (this.children[i].value === member) {
-//         return this.children[i];
-//       }        
-//     }
-//   }
-
-//   log() {
-//     this.logCalls++;
-//     let tree = `-- ${this.value}\n`;
-//     if (this.children.length > 0) {
-//       this.children.forEach(function(child) {
-//         if (child.children.length > 0) {
-//           for (let i = 0; i < this.logCalls; i++) {
-//             tree += `--`;
-//           }                   
-//           tree += `${child.log()}`
-//         } else {
-//           for (let i = 0; i < this.logCalls; i++) {
-//             tree += `--`;
-//           }
-//             tree += `-- ${child.value}\n`;
-//           }
-//       })
-//     }
-//     this.logCalls--;
-//     return tree;
-//   }
-// }
 
 
 
